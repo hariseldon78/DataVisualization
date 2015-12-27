62aae95fe64dba7f65dd5545e3e120d0c0cc1769
Roberto Previdi <hariseldon78@gmail.com>
2015-12-27 03:24:13 +0100

    first file

---
+++ Pod/Classes/DataVisualization.swift
@@ -0,0 +1,12 @@
+//
+//  DataVisualization.swift
+//  DataVisualization
+//
+//  Created by Roberto Previdi on 27/12/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+protocol Visualizable {}
+
