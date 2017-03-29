95c2945fce3cdc390f686a494e86e1320d252630
Roberto Previdi <hariseldon78@gmail.com>
2017-03-29 21:02:08 +0200

    refactor workspace

---
+++ Pod/Classes/DataVisualization/DataVisualization/DataVisualization.h
@@ -0,0 +1,19 @@
+//
+//  DataVisualization.h
+//  DataVisualization
+//
+//  Created by Roberto Previdi on 29/03/17.
+//  Copyright © 2017 fishdevlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataVisualization.
+FOUNDATION_EXPORT double DataVisualizationVersionNumber;
+
+//! Project version string for DataVisualization.
+FOUNDATION_EXPORT const unsigned char DataVisualizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataVisualization/PublicHeader.h>
+
+
